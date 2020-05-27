770307086cdca132a68902d079efcbdb0203d6e4
armand chariot <armand@chariot.be>
2020-05-27 12:30:17 +0200

    design the index page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,6 @@
 class PagesController < ApplicationController
   def index
-    # Test geocoding services locally by hardcoding latitude and longitude
-    if Rails.env.development? || Rails.env.test?
-      visitor_latitude = 34.05223 
-      visitor_longitude = -118.24368
-    else
-      visitor_latitude = request.location.latitude
-      visitor_longitude = request.location.longitude
-    end
-
-    @events = Event.featured(visitor_latitude, visitor_longitude)
+    @events = Event.all
   end
 
   def about
